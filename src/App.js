c8248129654f7c5a0f58bcb3d1eb897abed4e968
soumen sarkar <soumensarkar@soumens-MacBook-Pro.local>
2025-01-14 18:29:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Weather from "./weather.jsx";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
